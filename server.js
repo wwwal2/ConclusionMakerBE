4097cdd6f0020f7c7d5d6c7eccdc7191dff6c561
wwwal2 Al2 <smalex9@gmail.com>
2026-03-14 15:15:15 +0200

    update port

--- server.js
+++ server.js
@@ -10,42 +10,8 @@ const FILES_DIR = path.join(__dirname, '../../data/files');
 const TEMPLATE_DIR = path.join(__dirname, '../../data/template');
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:3000',
 }))
-
-// app.get('/api/template1', (req, res) => {
-//   const templatePath = path.join(TEMPLATES_DIR, 'templateDOCX.docx')
-
-//     res.setHeader(
-//     "Content-Type",
-//     "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-//   );
-//   res.setHeader("Content-Disposition", "attachment; filename=template1.docx");
-
-//     res.sendFile(templatePath, (err) => {
-//     if (err) {
-//       console.error("Error sending template:", err.message);
-//       res.status(404).send("Template not found");
-//     }
-//   });
-// })
-
-// app.get('/api/data-source-excel', (req, res) => {
-//   fs.readdir(DATA_SOURCE_DIR, (err, files) => {
-//     if (err) {
-//       return res.status(500).send('Cannot read dataSource folder')
-//     }
-
-//     const excelFile = files.find((f) => /\.(xls|xlsx|xlsm)$/i.test(f))
-
-//     if (!excelFile) {
-//       return res.status(404).send('No Excel file found in dataSource')
-//     }
-
-//     const fullPath = path.join(DATA_SOURCE_DIR, excelFile)
-//     res.download(fullPath, excelFile)
-//   })
-// })
 
 app.get('/api/template', (req, res) => {
   fs.readdir(TEMPLATE_DIR, (err, files) => {
